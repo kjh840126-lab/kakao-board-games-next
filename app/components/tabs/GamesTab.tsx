ea0cb5abbfdd7713ce35966fab0ca15caf61c7b2
hayden.hoi <hayden.hoi@kakaocorp.com>
2026-08-10 18:08:05 +0900

    Update game admin description and button label

--- app/components/tabs/GamesTab.tsx
+++ app/components/tabs/GamesTab.tsx
@@ -192,7 +192,7 @@ export const GamesTab = memo(({
                         onClick={() => toggleCartItem(game)} 
                         className={`px-3.5 py-[7.5px] rounded-xl font-bold text-xs cursor-pointer ${
                           isSelectedInCart 
-                            ? 'bg-slate-900 text-white dark:!border-none' 
+                            ? 'bg-slate-900 text-white !dark:border-none' 
                             : 'bg-[#FEE500] text-slate-900 dark:bg-amber-850 dark:text-white'
                         }`}
                       >
